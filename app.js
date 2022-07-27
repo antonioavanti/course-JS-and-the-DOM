66e74340c416fddd7f9e8f52ef15f493bf61f12f
ANTHONY GATHOGO <106891513+antonioavanti@users.noreply.github.com>
2022-07-27 18:28:21 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+document.addEventListener('click', function(){
+	const mainHeading = document.querySelector('h1');
+
+	mainHeading.setAttribute("style","background-color: red;");
+})
+
+/*const myCustomDiv = document.createElement('div');
+
+for (let i = 1; i <= 200; i++) {
+    const newElement = document.createElement('p');
+    newElement.textContent = 'This is paragraph number ' + i;
+
+    newElement.addEventListener('click', function respondToTheClick(evt) {
+        console.log('A paragraph was clicked.');
+    });
+
+    myCustomDiv.appendChild(newElement);
+}
+
+document.body.appendChild(myCustomDiv);
+*/
